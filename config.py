3dbc62dc56a39c1d1b878ce1b364e4b4e2dec214
Dinesh <dcpandey89@gmail.com>
2026-07-11 12:04:53 +0530

    main branch | push merge conflict test

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ database=db_main_change2
 
 def learnMerge():
         """This function is to test how to handle merge conflict"""
-        mergeTime = 30
-        dbname = "main db name"
-        message = "this is original main config"
+        mergeTime = 60
+        dbname = "main db name 2"
+        message = "this is modified main config"
 
