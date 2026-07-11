1771b3c0509a2abe929e7b7e32a83bcaf70acc9b
Dinesh <dcpandey89@gmail.com>
2026-07-11 11:57:35 +0530

    main branch | a fresh commit to learn merge

--- config.py
+++ config.py
@@ -1,3 +1,11 @@
 time=30
 
 database=db_main_change
+
+def learnMerge():
+        """This function is to test how to handle merge conflict"""
+        mergeTime = 30
+        dbname = "main db name"
+        message = "this is original main config"
+
+
