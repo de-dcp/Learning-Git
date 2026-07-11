5b5bcee4770852c044feade3705639139f81fb3e
Dinesh <dcpandey89@gmail.com>
2026-07-11 12:05:59 +0530

    "learn-merge branch | push merge conflict test"

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ database=db_main_change2
 
 def learnMerge():
         """This function is to test how to handle merge conflict"""
-        mergeTime = 30
-        dbname = "main db name"
-        message = "this is original main config"
+        mergeTime = 90
+        dbname = "merge db name"
+        message = "this is merge branch config"
 
